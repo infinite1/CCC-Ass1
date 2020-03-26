590ce3324082c4958e99374f99b64c6a44d36b97
Xinyu <victorvone@gmail.com>
2020-03-26 16:49:26 +1100

    Initialize project

---
+++ script.py
@@ -0,0 +1,52 @@
+import argparse
+import json
+import os
+import time
+import heapq
+
+def valid_file(filename):
+    """Check if the input file is a json file"""
+    base, ext = os.path.splitext(filename)
+    if ext.lower() != '.json':
+        raise argparse.ArgumentTypeError('file must have a json extension')
+    return filename
+
+# record start time
+start_time = time.time()
+
+# parse command line arguments
+parser = argparse.ArgumentParser("python3 script.py")
+parser.add_argument('filename', type=valid_file,
+                    help='twitter json file to process')
+args = parser.parse_args()
+filename = args.filename
+
+# record frequency of hashtags
+hashtags_table = {}
+
+with open(filename) as data:
+    for line in data:
+        line = line.strip()
+        try:
+            content = json.loads(line[0:len(line)-1])
+
+            hashtags = content["doc"]["entities"]["hashtags"]
+            if len(hashtags) > 0:
+                for hashtag in hashtags:
+                    text = "#" + hashtag["text"]
+                    if text not in hashtags_table:
+                        hashtags_table[text] = 0
+                    hashtags_table[text] += 1
+            print(content["doc"]["text"], content["doc"]["lang"])
+
+        except:
+            # skip lines that are not formatted correctly in json
+            # print("invalid json format")
+            continue
+
+topTenHashtags = heapq.nlargest(10, hashtags_table.items(), key=lambda i: i[1])
+print(topTenHashtags)
+
+# calculate exucation time
+duration = time.time() - start_time
+print("\nThe program uses {0:.2f} seconds".format(duration))
